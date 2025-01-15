09c8ad4d8f36c1df73a25a1585f7fa51fcb1abba
GoldyRoger07 <latouche822@gmail.com>
2025-01-15 01:36:17 -0500

    test4

--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -7,7 +7,7 @@ export class UrlService {
 
   // serverUrl = "http://192.168.0.196:8080"
   // serverUrl = "http://localhost:8080"
-  serverUrl = "https://9247-190-102-77-71.ngrok-free.app"
+  serverUrl = "https://sure-wired-redfish.ngrok-free.app"
 
   baseUrl = this.serverUrl+"/api/raf"
   // baseUrl = "http://192.168.0.178:8080/api/raf"
